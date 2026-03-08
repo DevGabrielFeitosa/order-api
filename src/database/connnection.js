3f52002064c7fdafbb3980e8fd3b0f0e26489658
Feitosa <feitosawj@gmail.com>
2026-03-08 16:39:22 -0300

    FEAT - adicionando conexão do banco de dados

---
+++ src/database/connnection.js
@@ -0,0 +1,11 @@
+const { Pool } = require("pg")
+
+const pool = new Pool({
+    host: "localhost",
+    user: "postgres",
+    password: "postgres",
+    database: "ordersdb",
+    port: 5433
+})
+
+module.exports = pool

3d27b8cf2c2e26c45357c692a3845d6323f64bc8
Feitosa <feitosawj@gmail.com>
2026-03-09 10:47:28 -0300

    FEAT - nova rota para API de Get order By Id

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const orderController = require("../controllers/orderController")
 
 router.post("/", orderController.createOrder)
+router.get("/:orderId", orderController.getOrderById)
 
 module.exports = router
